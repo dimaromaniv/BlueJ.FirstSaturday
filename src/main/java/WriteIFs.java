a27915840b31a4df1da91d43ef999a71c12cb382
Dmytro_Romaniv <128440171+dimaromaniv@users.noreply.github.com>
2023-07-04 17:26:43 -0400

    Update WriteIFs.java

--- src/main/java/WriteIFs.java
+++ src/main/java/WriteIFs.java
@@ -3,49 +3,67 @@ package src.main.java;
 /**
  * Write a description of class src.main.java.WriteIFs here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Dmytro Romaniv)
+ * @version (v 0.01 07/03/2023)
  */
 public class WriteIFs
 {
- 
+
     public void playerDied(boolean player1) {
-        // Write an IF statement that checks “player1.isAlive()” 
+        if(!isAlive(player1)) {
+            displayGameOver(player1);
+        }
+
+        // Write an IF statement that checks “player1.isAlive()”
         // and if that’s false, calls “displayGameOver(player1)”
-     
+
     }
-    
+
     public String thermoSTAT(int room) {
-        // Write an IF statement that checks the 
-        // “temperature(room)” and if that check is less than 70, 
+
+        if(tempurature(room) < 70 ) {
+            heatOn();
+        }
+        else {coolOn();}
+        // Write an IF statement that checks the
+        // “temperature(room)” and if that check is less than 70,
         // calls “heatOn()” else calls “coolOn()”
 
 
-        
+
         return this.ss;
     }
 
     public void fireplaceControl(Object fireplace1) {
-        // Write an IF statement that checks 
-        // “outsideTemp()” is less than 50 
-        // AND 
-        // “insideTemp()” is less than 62, 
+
+        if(outsideTemp() < 50 && insideTemp() < 62 ) {
+            startAFire(fireplace1);
+        }
+
+        // Write an IF statement that checks
+        // “outsideTemp()” is less than 50
+        // AND
+        // “insideTemp()” is less than 62,
         // calls “startAFire(fireplace1)”
 
     }
 
     public void checkFuel(double fuelLevel) {
-        // Write an IF statement that checks “fuelLevel” 
+
+        if (fuelLevel < 0.08 ) {
+            refuel();
+        }
+        // Write an IF statement that checks “fuelLevel”
         // and if that check is less than 0.08, calls “refuel()”
 
     }
 
 
-    
+
     /**
      *  Pay no attention to the code below this point.
      *
-     * 
+     *
      * instance variables
      */
     public int x;
@@ -55,19 +73,19 @@ public class WriteIFs
     public String ss;
 
 
-  /**
-   * Constructor for objects of class src.main.java.WriteIFs
-   */
-  public WriteIFs()
-  {
-      // initialise instance variables
-      x = 0;
-      tt_t = 0;
-      tt_s = 1;
-      ss = "";
-      oo1 = 61;
-      oo2 = 49;
-  }
+    /**
+     * Constructor for objects of class src.main.java.WriteIFs
+     */
+    public WriteIFs()
+    {
+        // initialise instance variables
+        x = 0;
+        tt_t = 0;
+        tt_s = 1;
+        ss = "";
+        oo1 = 61;
+        oo2 = 49;
+    }
 
     // associated routines
     public boolean isAlive(boolean p) {
@@ -82,7 +100,7 @@ public class WriteIFs
     private void coolOn() {
         this.ss = "cooling";
     }
- 
+
     private int insideTemp() {
         return oo1;
     }
